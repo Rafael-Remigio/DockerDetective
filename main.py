9d036cc04b4a6f59905193396400d437c1392558
rafael <rafael.lixa@ua.pt>
2023-12-05 19:40:46 +0000

    Add inspection page

--- main.py
+++ main.py
@@ -10,13 +10,24 @@ base_url = 'http://localhost:2375'
 http_utils = HttpUtils(base_url)
 
 @app.route('/')
-def hello():
+def index():
 
    images = http_utils.get_json_from_endpoint("images/json")
 
    containers = http_utils.get_json_from_endpoint("containers/json?all=true")
 
    return render_template("index.html",images=images,containers=containers)
+
+
+
+@app.route('/container/<container_id>')
+def inspect_container(container_id):
+
+   # Fetch detailed data
+   endpoint = "container/" + container_id + "/json" 
+   data = http_utils.get_json_from_endpoint(endpoint)
+
+   return render_template('container_inspection.html', container_data=data)
 
 # main driver function
 if __name__ == '__main__':
